0ff98336e6d74494b4a3d3820134f04f004644fc
Ylan1987 <ylanar@gmail.com>
2025-09-24 19:10:49 -0300

    BUG en imposition_page

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,12 @@ import { createClient } from '@supabase/supabase-js';
 import { ImpositionPage } from './ImpositionPage';
 import { Workspace } from './Workspace';
 import { pdfjs } from 'react-pdf';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css'; // Importa los estilos
-import 'react-pdf/dist/esm/Page/TextLayer.css';     // Importa los estilos
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+// --- CONFIGURACIÓN DEL WORKER COMPATIBLE CON WEBPACK ---
+import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
+pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 // --- SUPABASE CLIENT SETUP ---
 // NOTE: These variables should be set in your hosting environment (e.g., Vercel).
