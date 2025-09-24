893507e5769b8cf248ae4f0598d5e2bac8ebd0b5
Ylan1987 <ylanar@gmail.com>
2025-09-24 14:09:16 -0300

    BUG en imposition_page

--- src/ImpositionPage.jsx
+++ src/ImpositionPage.jsx
@@ -6,10 +6,7 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 // --- Componentes Auxiliares (sin cambios) ---
 const SearchResultCard = ({ quote, onSelect }) => (
